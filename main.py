22ec4bcfb368927a227ba4e0be6cc4f853e30d18
evgen <evgenijzornickij@gmail.com>
2024-12-15 18:34:34 +0200

    add funny story

---
+++ main.py
@@ -0,0 +1,15 @@
+def print_childhood_story():
+    print("When I was a child, I loved climbing trees to see how high I could get.")
+
+def print_funny_story():
+    print("")
+
+
+if __name__ == "_main_":
+    print("My funny story:")
+    print_funny_story()
+
+    print("Teamate's funny story:")
+
+    print("My childhood memory:")
+    print_childhood_story()
